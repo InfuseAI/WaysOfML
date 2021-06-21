8ca3f2854f632bbcfe00ec0ce7bc8417d258a100
Poga Po <hi@devpoga.org>
2021-06-21 23:12:25 +0800

    better og image

--- gen-og-image.js
+++ gen-og-image.js
@@ -6,10 +6,15 @@ const Screenshot = async () => {             // Define Screenshot function
   const page = await browser.newPage();      // Open new page
   await page.setViewport({width: 1280, height: 1024})
   await page.goto(url);                      // Go website
-  const logo = await page.$('h1');        // logo is the element you want to capture
-  await logo.screenshot({
-    path: 'public/og.png'
-    });
+  await page.screenshot({
+    path: 'public/og.png',
+    clip: {
+      x: 0,
+      y: 0,
+      width: page.viewport().width,
+      height: page.viewport().height * 0.8
+    }
+  });
   await page.close();                        // Close the website
   await browser.close();                     // Close the browser
 }
