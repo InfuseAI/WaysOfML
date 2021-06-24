5c978a07e4d6ed29033da10a3bf611d1ec09140c
Poga Po <hi@devpoga.org>
2021-06-24 22:15:59 +0800

    change divider style

--- pages/divider.js
+++ pages/divider.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Divider() {
   return (
-    <div className="max-w-xs mx-auto mt-8 mb-12 border-b border-gray-200">
+    <div className="max-w-xs mx-auto mt-16 mb-16 text-center text-gray-300">
       &nbsp;
+      §
     </div>
   );
 }
