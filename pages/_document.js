5171ebaaecfbe5ee30d8b754a2a19eed493eafa5
Jie Peng <im@jiepeng.me>
2021-06-28 18:42:01 +0800

    fix(ga): revert something
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { GTM_ID } from '../lib/gtm';
+
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 export default class MyDocument extends Document {
   render() {
@@ -15,35 +16,37 @@ export default class MyDocument extends Document {
           <meta property="og:type" content="article" />
           <meta property="og:description" content="Powered by InfuseAI" />
 
-          {/* gtm */}
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', '${GTM_ID}');
-              `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
             }}
           />
 
           {/* plausible */}
-          <script defer data-domain="waysof.ml" src="https://plausible.io/js/plausible.outbound-links.js"></script>
+          <script
+            defer
+            data-domain="waysof.ml"
+            src="https://plausible.io/js/plausible.outbound-links.js"
+          ></script>
         </Head>
+
         <body>
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
