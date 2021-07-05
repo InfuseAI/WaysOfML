0c85a0a1d80627f3b11d9b5cacd9776938636227
poga <poga@users.noreply.github.com>
2021-07-05 06:10:46 +0000

    deploy: 4fddff6c871c59ea11777dd4bd308fdb8edbad5a

---
+++ _next/static/bIrF3cVDawqp_Mcmwz2M_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2a28d0443948f8efec57.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
