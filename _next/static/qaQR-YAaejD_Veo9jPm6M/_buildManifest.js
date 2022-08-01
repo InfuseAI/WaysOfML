4f143444006583496f025f73d1bfba68c6c7a3b1
hlb <hlb@users.noreply.github.com>
2022-08-01 04:06:23 +0000

    deploy: b703baabff7eba607c730dce4a08eb9da376c944

---
+++ _next/static/qaQR-YAaejD_Veo9jPm6M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-75a411deb9d4708e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
