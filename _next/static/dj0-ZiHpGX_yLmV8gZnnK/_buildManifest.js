f6615e6a6c5aeef236f65d54fb6aa87572a4e4fe
neighborhood999 <neighborhood999@users.noreply.github.com>
2022-08-04 08:59:31 +0000

    deploy: dab2bd3c6f05287e0f4c677c3b067470ee99a12b

---
+++ _next/static/dj0-ZiHpGX_yLmV8gZnnK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c5f70fff069dabd9.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/qaQR-YAaejD_Veo9jPm6M/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-75a411deb9d4708e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
