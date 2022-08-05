d45079083ddea0cc5d4d39c46f3baa60703e6b76
neighborhood999 <neighborhood999@users.noreply.github.com>
2022-08-05 00:04:37 +0000

    deploy: c5019dc9ac1449d49cdd2ea2fc53a0093e3e5e2a

---
+++ _next/static/LvAh-OIc4vsA5PwENgvPR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-edd86168971be00e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
